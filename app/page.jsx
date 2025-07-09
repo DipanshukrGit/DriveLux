657cb1a82800e79469dacd214e39ee90143717ca
Dipanshu Kumar <dipanshuhub0411@gmail.com>
2025-07-10 00:03:10 +0530

    Updated code with recent changes

---
+++ app/page.jsx
@@ -0,0 +1,51 @@
+import CarCard from "@/components/CarCard";
+import HomeSearch from "@/components/home-search";
+import { Button } from "@/components/ui/button";
+import { featuredCars } from "@/lib/data";
+import { ChevronRight } from "lucide-react";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col pt-20">
+
+      {/* Hero Section with Gradient Title */}
+      <section className="relative py-16 md:py-28 dotted-background">
+        <div className="max-w-4xl mx-auto text-center">
+          <div className="mb-8">
+            <h1 className="text-5xl md:text-8xl mb-4 gradient-title">
+              Find your Dream Car with DriveLux_Ai
+            </h1>
+            <p className="text-xl text-gray-500 mb-8 max-w-2xl mx-auto">
+              Advanced AI Car Search and test drive from thousands of vehicles.
+            </p>
+          </div>
+
+          {/* Search Component (Client) */}
+          <HomeSearch />
+        </div>
+      </section>
+
+       {/* Featured Cars */}
+      <section className="py-12">
+        <div className="container mx-auto px-4">
+          <div className="flex justify-between items-center mb-8">
+            <h2 className="text-2xl font-bold">Featured Cars</h2>
+            <Button variant="ghost" className="flex items-center" asChild>
+              <Link href="/cars">
+                View All <ChevronRight className="ml-1 h-4 w-4" />
+              </Link>
+            </Button>
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            {featuredCars.map((car) => (
+              <CarCard key={car.id} car={car} />
+            ))}
+          </div>
+        </div>
+      </section>
+
+
+</div>
+  );
+}
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,51 +1,80 @@
-
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { SignedIn, SignedOut, SignInButton, SignOutButton, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { Button } from "./ui/button";
-import { CarFront, Heart } from "lucide-react";
+import { ArrowLeft, CarFront, Heart, Layout } from "lucide-react";
 
-const Header = async({isAdminPage=false}) => {
-  const isAdmin=false;
+const Header = async ({ isAdminPage = false }) => {
+  const isAdmin = false;
 
   return (
-   <header className="fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 border-b">
-    <nav className="mx-auto px-4 py-4 flex items-center justify-between h-12  w-full">
-      <Link href={isAdminPage ? "/admin" : "/"}>
- <Image
-  src="/logo2.png"
-  alt="DriveLux Logo"
-  width={100}
-  height={60}
-  className=" object-contain"
-/>
-
-            {isAdminPage && (
+    <header className="fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 border-b">
+      <nav className="mx-auto px-4 py-4 flex items-center justify-between">
+        <Link href={isAdminPage ? "/admin" : "/"} className="flex">
+          <Image
+            src={"/logo.png"}
+            alt="DriveLux Logo"
+            width={200}
+            height={60}
+            className="h-12 w-auto object-contain"
+          />
+          {isAdminPage && (
             <span className="text-xs font-extralight">admin</span>
           )}
-      </Link>
+        </Link>
+        <div className="flex items-center space-x-4">
+          {isAdminPage ? (
+            <>
+              <Link href="/">
+                <Button variant="outline" className="flex items-center gap-2">
+                  <ArrowLeft size={18} />
+                  <span>Back To App</span>
+                </Button>
+              </Link>
+            </>
+          ) : (
+            <SignedIn>
+              <Link href="/saved-cars">
+                <Button>
+                  <Heart size={18} />
+                  <span className="hidden md:inline"> Saved Cars</span>
+                </Button>
+              </Link>
 
-      <div className="flex items-center space-x-4">
-      <SignedIn>
-          <Link href="/reservations">
-            <Button variant="outline">
-              <CarFront size={18}/>
-              <span className="hidden md:inline"> My Reservation</span>
-            </Button>
-            </Link>
+              {!isAdmin ? (
+                <Link href="/reservations">
+                  <Button variant="outline">
+                    <CarFront size={18} />
+                    <span className="hidden md:inline"> My Reservation</span>
+                  </Button>
+                </Link>
+              ) : (
+                <Link href="/admin">
+                  <Button variant="outline">
+                    <Layout size={18} />
+                    <span className="hidden md:inline"> Admin Portal</span>
+                  </Button>
+                </Link>
+              )}
+            </SignedIn>
+          )}
 
+          <SignedOut>
+            <SignInButton forceRedirectUrl="/">
+<Button variant='outline'>Login</Button>
+            </SignInButton>
+          </SignedOut>
 
-            <Link href="/saved-cars">
-            <Button>
-              <Heart size={18}/>
-              <span className="hidden md:inline"> Saved Cars</span>
-            </Button>
-            </Link>
-          </SignedIn>
-      </div>
-    </nav>
-   </header>
+          <SignedIn><UserButton
+          appearance={{
+            elements:{
+              avatarBox:"w-10 h-10"
+            },
+          }}/></SignedIn>
+        </div>
+      </nav>
+    </header>
   );
 };
 
